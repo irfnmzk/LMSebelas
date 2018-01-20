1e17d881b93b76b2c8b1a5cd6333143521883308
unknown <fachrulyusuf.mubaroq@gmail.com>
2018-01-20 20:32:56 +0700

    menambah table materi

---
+++ database/migrations/2018_01_20_132900_create_materi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMateriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materi', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('judul');
+            $table->string('deskripsi');
+            $table->string('kelas_id');
+            $table->string('creator_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materi');
+    }
+}
