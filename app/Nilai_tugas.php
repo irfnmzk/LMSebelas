46a0076566725675f9db51a1a78e20e1a23461d7
unknown <fachrulyusuf.mubaroq@gmail.com>
2018-01-21 18:50:32 +0700

    menambah nilai_tugas

---
+++ app/Nilai_tugas.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Nilai_tugas extends Model
+{
+    protected $fillable = [
+    	'jawaban_tugas_id','nilai', 'ket'
+    ];
+
+    public function jawaban_tugas()
+    {
+    	return $this->belongsTo('App\Jawaban_tugas','jawaban_tugas_id');
+    }
+}
