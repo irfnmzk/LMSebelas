38291b85c99a3c2b1db04566a4c8a02710ab1663
Irfan Marzuki <Ragnaboyx@gmail.com>
2018-02-17 12:13:36 +0700

    fix typo on diskusi migration

--- database/migrations/2018_02_11_074915_create_diskusis_table.php
+++ database/migrations/2018_02_11_074915_create_diskusis_table.php
@@ -29,6 +29,6 @@ class CreateDiskusisTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('diskusis');
+        Schema::dropIfExists('diskusi');
     }
 }
