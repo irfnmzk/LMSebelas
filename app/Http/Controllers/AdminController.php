897b00ebee4e65063e22febf7ca07b828c196bf4
Irfan Marzuki <ragnaboyx@gmail.com>
2018-04-09 11:12:07 +0000

    data user dan hapus user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Kelas;
+use App\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -21,6 +22,18 @@ class AdminController extends Controller
     public function deleteKelas($id){
         $kelas = Kelas::findOrFail($id);
         $kelas->delete();
+        return redirect()->back();
+    }
+    
+    public function allUser(){
+        $user = User::all();
+        
+        return view('admin.user', compact('user'));
+    }
+    
+    public function deleteUser($id){
+        $user = User::findOrFail($id);
+        $user->delete();
         return redirect()->back();
     }
 }
