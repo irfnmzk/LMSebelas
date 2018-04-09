c7be5356d84af609f50ac85e05d2b6bd4cc1c002
Irfan Marzuki <ragnaboyx@gmail.com>
2018-04-09 13:46:48 +0000

    edit data user

--- resources/views/admin/usermenu.blade.php
+++ resources/views/admin/usermenu.blade.php
@@ -1,22 +1,18 @@
-<div>
-    <table>
-        <form method="POST" action="{{route('admin.user.pass', $id)}}">
-            <tr>
+            
+                <form method="POST" action="{{route('admin.user.pass', $id)}}" onSubmit="return confirm('Anda yakin dengan operasi ini?')">
+                {{csrf_field()}}
                 <td>Ubah Password</td>
                 <td><input type="text" name="password"/></td>
-                <td><input type="submit" value="Submit"/></td>
-            </tr>
-        </form>
-        <form method="POST" action="{{route('admin.user.role', $id)}}">
-            <tr>
+                <td><input type="submit" value="Submit" /></td>
+                </form>
+
+                <form method="POST" action="{{route('admin.user.role', $id)}}" onSubmit="return confirm('Anda yakin dengan operasi ini?')">
+                {{csrf_field()}}
                 <td>Ubah Role</td>
-                <td><select>
+                <td><select name="role">
                     <option value="1">Guru</option>
                     <option value="2">Siswa</option>
                     <option value="3">Admin</option>
                 </select></td>
-                <td><input type="submit" value="Submit"/></td>
-            </tr>
-        </form>
-    </table>
-</div>
+                <td><input type="submit" value="Submit" /></td>
+                </form>
